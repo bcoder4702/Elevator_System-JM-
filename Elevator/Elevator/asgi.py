b46449c53579bf2ecc9108f96c0013c5676de843
Bitthal verma <87093213+bcoder4702@users.noreply.github.com>
2024-03-08 15:46:15 +0530

    project setup

--- Elevator/Elevator/asgi.py
+++ Elevator/Elevator/asgi.py
@@ -4,7 +4,7 @@ ASGI config for Elevator project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
@@ -14,3 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Elevator.settings')
 
 application = get_asgi_application()
+
